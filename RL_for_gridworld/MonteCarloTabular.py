0fd8d310842e9cdaf85c9b3fd6cab6d8d5bf11e1
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-08-05 19:29:53 -0400

    Updated the future discounted reward calculation.

--- RL_for_gridworld/MonteCarloTabular.py
+++ RL_for_gridworld/MonteCarloTabular.py
@@ -18,7 +18,9 @@ class MonteCarloTabular():
         return self.state_values[self.convert_to_immutable(state)]
     
     def batch_update(self, states, rewards, actions):
-        """Update the model using the states, rewards."""
+        """Update the model using the states, rewards using the iterative MC update.
+            V(s_t) = V(s_t) + alpha(G_t - V(s_t))
+        """
         discounted_rewards = self.discount_reward(rewards)
         for state, reward in zip(states, discounted_rewards):
             self.state_values[self.convert_to_immutable(state)] = self.state_values[self.convert_to_immutable(state)] + self.alpha(reward - self.get_value(state))
@@ -30,7 +32,13 @@ class MonteCarloTabular():
     
     def discount_reward(self, rewards):
         """Calculate the future discounted reward."""
-        return rewards
+        discounted_rewards = []
+        for _ in range(len(rewards)):
+            temp_r = 0
+            for index, r in enumerate(rewards):
+                temp_r += (self.gamma**index)*r
+            discounted_rewards.append(temp_r)
+        return discounted_rewards
 
 
 
