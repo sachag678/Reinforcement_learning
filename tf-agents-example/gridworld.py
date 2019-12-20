281ba22bea8503c87a418b76e667ab817c1718fb
Karishnu Poddar <karishnu@gmail.com>
2019-12-20 20:25:41 +0530

    Change imports in tf-agents example to support updated version

--- tf-agents-example/gridworld.py
+++ tf-agents-example/gridworld.py
@@ -6,9 +6,9 @@ from tf_agents.environments import py_environment
 from tf_agents.environments import tf_environment
 from tf_agents.environments import tf_py_environment
 from tf_agents.environments import utils
-from tf_agents.specs import array_spec
-from tf_agents.environments import time_step as ts
 from tf_agents.environments import wrappers
+from tf_agents.specs import array_spec
+from tf_agents.trajectories import time_step as ts
 
 tf.compat.v1.enable_v2_behavior()
 
